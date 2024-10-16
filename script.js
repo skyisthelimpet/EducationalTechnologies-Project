46c311b6aeada4ed4309d58ddd49d02906917462
İslam Gözhamam <awoll.rur@gmail.com>
2024-10-16 09:56:47 +0300

    fixes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const menuButton = document.getElementById("nav_bar_icon");
 
 window.onload = function () {
-    menuButton.checked = false;
+    menuButton.checked = false; //TO BE SURE THAT ICON IS RESET AFTER REFRESH
 };
 
 menuButton.addEventListener('change', () => {
     document.getElementById("side-menu").classList.toggle('w-0');
-    document.getElementById("side-menu").classList.toggle('w-44');
-});
+    document.getElementById("side-menu").classList.toggle('w-44'); // we are just toggling width classes of our side menu 
+}); 
