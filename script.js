d9e530139b6bb6d1499145ce6ffb7c1900e1c54c
İslam Gözhamam <awoll.rur@gmail.com>
2024-10-30 21:29:18 +0300

    added about me and some upgrades to main page

--- script.js
+++ script.js
@@ -16,3 +16,42 @@ document.getElementById("blog-btn").addEventListener('click', () => {
 })
 
 
+
+
+async function fetchTodaysFact() {
+    try {
+        const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/today', {
+            'Accept': 'text/plain'
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+
+        console.log(data);
+        displayFact(data);
+    } catch (error) {
+        console.error('Error fetching today\'s facts:', error);
+        const factElement = document.getElementById('fact-text');
+        if (factElement) {
+            factElement.textContent = 'The word "set" has more definitions than any other word in the English language.';
+        }
+    }
+}
+
+
+function displayFact(data) {
+
+    const factElement = document.getElementById('fact-text');
+    if (factElement) {
+        factElement.textContent = data.text;
+    }
+}
+
+window.addEventListener('load', fetchTodaysFact);
+
+
+
