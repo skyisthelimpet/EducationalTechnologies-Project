2778f12d6b5e95b3017fc7748c2e0e3881a08e28
İslam Gözhamam <awoll.rur@gmail.com>
2024-11-16 22:30:43 +0300

    FINALLY ITS DONE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./blog.html", "./aboutme.html" , "./wordcloud.html", "./games.html", "./read.html",
-              "./articles/article1.html"
+              "./articles/article1.html",
+              "./articles/article2.html",
+              "./articles/article3.html",
+              "./watch.html"
   ],
   theme: {
     extend: {},
